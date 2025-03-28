74a5227cb6411514ccdf0aed0c73a1ba5419dd8d
Mudit Sharma <115790300+mudit203@users.noreply.github.com>
2025-03-28 14:59:37 +0530

    configured shadcn configured tailwind css

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+const Signup = () => {
+  return (
+    <div>
+      hi this is signupp
+      <Button> kyon </Button>
+    </div>
+  )
+}
+
+export default Signup
